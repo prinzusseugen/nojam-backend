dafa48ee032a14c360733c9724dc6eb1d564c99c
wongwanchoi <prinzusseugen@gmail.com>
2024-02-26 14:56:37 +0900

    init commit

---
+++ nojam/src/main/java/com/soos/nojam/NojamApplication.java
@@ -0,0 +1,13 @@
+package com.soos.nojam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NojamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NojamApplication.class, args);
+	}
+
+}
---
+++ nojam/src/test/java/com/soos/nojam/NojamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soos.nojam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NojamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
