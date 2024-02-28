420295d31a87545b7e76f7004a7444d2c2879a2a
wongwanchoi <prinzusseugen@gmail.com>
2024-02-28 19:51:43 +0900

    Clean: 필요없는 import 제거

--- nojam/src/main/java/com/soos/nojam/global/auth/PasswordEncoder.java
+++ nojam/src/main/java/com/soos/nojam/global/auth/PasswordEncoder.java
@@ -38,7 +38,6 @@ public class PasswordEncoder {
         return new String(Hex.encode(result));
     }
     public boolean verifyPassword(String password, String encodedPassword){
-        byte[] decodedPassword = Hex.decode(encodedPassword);
         String newEncodedPassword = encodePassword(password);
         return newEncodedPassword.equals(encodedPassword);
     }
