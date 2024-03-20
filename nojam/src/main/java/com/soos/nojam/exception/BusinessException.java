eb87e9a1ae7d8d7c9863f0ef44db10d2b2b6baa7
wongwanchoi <prinzusseugen@gmail.com>
2024-03-20 14:54:13 +0900

    feat: BusinessException 예외 추가

---
+++ nojam/src/main/java/com/soos/nojam/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.soos.nojam.exception;
+
+public class BusinessException extends RuntimeException {
+    private final ExceptionCode exceptionCode;
+
+    public BusinessException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+
+    public ExceptionCode getExceptionCode() {
+        return exceptionCode;
+    }
+}
